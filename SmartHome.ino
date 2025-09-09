d6fdad28d36d96a5efe77949b9faae7b3278553a
daryoush <daryoosh.safe@gmail.com>
2025-09-09 10:25:21 +0330

    fix: fix test case number in main file

--- SmartHome.ino
+++ SmartHome.ino
@@ -1,105 +1,64 @@
 
 // Auto-generated Arduino code
+#include <Servo.h>
+#include <DHT.h>
 
-
-enum State { IDLE, S1, S2, S3 };
+enum State { IDLE, GOOD, NEW };
 State currentState = IDLE;
 
+Servo myServo;
+DHT humSensor(A0, DHT11);
 
-float readTemperature(int pin) {
-  int v = analogRead(pin);
-  float voltage = v * (5.0 / 1023.0);
-  float value = voltage * 100.0; // LM35
-  Serial.print("temperature: ");
-  Serial.println(value);
-  return value;
+float readHumidity(DHT &sensor) {
+  float h = sensor.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println(" %");
+  return h;
 }
 
-float readLight(int pin) {
-  int value = analogRead(pin);
-  Serial.print("light: ");
-  Serial.println(value);
-  return value; // 0 (dark) to 1023 (bright)
+void moveServo(Servo &servo, int angle) {
+  if (angle < 0) angle = 0;
+  if (angle > 180) angle = 180;
+  servo.write(angle);
 }
 
-float readDistance(int TRIG_PIN, int ECHO_PIN) {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-
-  // Send 10 µs pulse
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  // Measure pulse duration
-  float duration = pulseIn(ECHO_PIN, HIGH);
-
-  // Calculate distance in cm
-  float distance = duration * 0.034 / 2;
-
-  // Print result
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  return distance;
-}
-
-bool readMotion(int pirPin) {
-int motion = digitalRead(pirPin);
-if (motion == HIGH) {
-Serial.println("Motion Detected!");
-return true;
-} else {
-Serial.println("No Motion");
-return false;
-}
+void sweepServo(Servo &servo, int start_angle, int end_angle, int step, int delay_ms) {
+  for(int pos = start_angle; pos <= end_angle; pos += step) {
+    servo.write(pos);
+    delay(delay_ms);
+  }
 }
 
 
 void setup() {
 Serial.println("Starting Program ...");
 Serial.begin(9600);
-pinMode(6, OUTPUT);
-pinMode(14, INPUT);
-pinMode(15, INPUT);
-pinMode(2, OUTPUT);
-pinMode(3, INPUT);
-pinMode(4, INPUT);
-
+humSensor.begin(); // Initialize DHT11 sensor
+myServo.attach(9,1000,2000);
 }
 
 void state_idle() {
-  
+  moveServo(myServo, 20);
 }
-void state_s1() {
-  analogWrite(6, 191);
-  analogWrite(10, 211);
-  analogWrite(9, 55);
-  analogWrite(5, 255);
+void state_good() {
+  moveServo(myServo, 120);
 }
-void state_s2() {
-  analogWrite(6, 128);
-  analogWrite(10, 40);
-  analogWrite(9, 120);
-  analogWrite(5, 20);
-}
-void state_s3() {
-  analogWrite(6, 64);
-  analogWrite(10, 255);
-  analogWrite(9, 0);
-  analogWrite(5, 255);
+void state_new() {
+  sweepServo(myServo,46,120,1,20);
 }
 
 void checkTransitions() {
   switch(currentState) {
+    case NEW:
+      if (readHumidity(humSensor) < 30) currentState = IDLE;
+      break;
     case IDLE:
-      if (readTemperature(14) > 3) currentState = S1;
-      break;
-    case S1:
-      if (readTemperature(14) > 5) currentState = S2;
-      break;
-    case S2:
-      if (readTemperature(14) > 7) currentState = S3;
+      if (readHumidity(humSensor) > 50) currentState = NEW;
       break;
   }
 }
@@ -107,7 +66,7 @@ void checkTransitions() {
 void loop() {
   checkTransitions();
   switch(currentState) {
-    case IDLE: state_idle(); break;case S1: state_s1(); break;case S2: state_s2(); break;case S3: state_s3(); break;
+    case IDLE: state_idle(); break;case GOOD: state_good(); break;case NEW: state_new(); break;
   }
   delay(200);
 }
